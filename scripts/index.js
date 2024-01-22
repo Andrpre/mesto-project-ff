3bb42b71fe4ee7ef142c7770727163ee5758f4c6
Andrey Kozhuhov <andrpre96@gmail.com>
2024-01-22 11:12:06 +0300

    Finish of the project

--- scripts/index.js
+++ scripts/index.js
@@ -1,33 +1,28 @@
-// @todo: Темплейт карточки
 const cardTemplate = document.querySelector("#card-template").content;
 const placesList = document.querySelector(".places__list");
-
-// @todo: DOM узлы
-
-// @todo: Функция создания карточки
 
 function creatinCards(name, link, deletingCards) {
     const cardElement = cardTemplate.querySelector(".places__item").cloneNode(true);
     cardElement.querySelector(".card__image").src = link;
+    cardElement.querySelector(".card__image").alt = name;
     cardElement.querySelector(".card__title").textContent = name;
 
     const deleteButton = cardElement.querySelector(".card__delete-button");
-    deleteButton.addEventListener("click", deletingCards);
+    deleteButton.addEventListener("click", function () {
+        deletingCards(cardElement);
+    });
 
-    console.log(cardElement);
     return cardElement;
 }
 
-// @todo: Функция удаления карточки
-function deletingCards() {
+function deletingCards(cardElement) {
+    cardElement.remove();
+}
+
+function outputCard(location) {
     initialCards.forEach(function (item) {
-        const listItem = deleteButton.closest('.places__item');
-        listItem.remove();
+        location.append(creatinCards(item.name, item.link, deletingCards));
     });
 }
 
-// @todo: Вывести карточки на страницу
-
-initialCards.forEach(function (item) {
-    placesList.append(creatinCards(item.name, item.link, deletingCards));
-});
+outputCard(placesList);
